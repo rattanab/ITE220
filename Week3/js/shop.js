7b29294752f95a34e143f251c98a3589a12a49a7
RATTANA-PC\Rattana <rattanabanchuen@gmail.com>
2016-12-06 18:32:39 +0700

    Week 3 & 4 Assigment

---
+++ Week3/js/shop.js
@@ -0,0 +1,7 @@
+var customerName = "Mintra";
+var price = 10;
+var quantity = 2;
+var totalPrice = price * quantity;
+
+var customerElement = document.getElementbyId("customer-name");
+customerElement.textContent = customerName;
