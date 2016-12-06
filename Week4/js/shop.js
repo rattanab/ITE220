b752384b7aee65a7476b1562fc8a34f9f0ca07a4
rattanab <rattanabanchuen@gmail.com>
2016-12-06 18:42:11 +0700

    Add files via upload
    Upload to sync this branch with the rest since I can't pull or get github to sync

---
+++ Week4/js/shop.js
@@ -0,0 +1,54 @@
+var customerName = "Rattana";
+// var price = 10;
+// var quantity = 2;
+// variables are not in use
+var totalPrice = 0;
+var products = [
+	"Brooklyn T-Shirt White",
+	"Brooklyn T-Shirt Black",
+	"Apple Watch",
+	"Android Phone"
+];
+var prices = [
+	10,
+	10,
+	199,
+	159,
+];
+var productsText = "";
+var productsElement = document.getElementById("product-list");
+for (var i = 0; i < products.length; i++){
+	productsText += "<li class='list-group-item'><span class='badge'>$" + prices[i] + "</span>" + products[i] + "</li>";
+	productsElement.innerHTML = productsText;
+}
+
+// Time dependent Greeting
+// document.getElementById("time-greeting").innerHTML = new Date().getHours();
+// use to check hours value
+
+if (new Date().getHours() < 12) {
+	document.getElementById("time-greeting").innerHTML = "Good morning";
+} else if (new Date().getHours() < 18) {
+	document.getElementById("time-greeting").innerHTML = "Good afternoon";
+} else {
+	document.getElementById("time-greeting").innerHTML = "Good evening";
+}
+
+var customerElement = document.getElementById("customer-name");
+customerElement.textContent = customerName;
+
+// Pricing
+
+var isDiscount = 1;
+var discountRate = 0.25;
+
+if (isDiscount == 1) {
+	totalPrice = prices[0] + prices[1] + prices[2] + prices[3];
+	totalPrice -= totalPrice*discountRate;
+	document.getElementById("discount-rate").innerHTML = "25% off";
+} else {
+	totalPrice = prices[0] + prices[1] + prices[2] + prices[3];
+}
+
+var totalPriceElement = document.getElementById("total-price");
+totalPriceElement.textContent = totalPrice;
